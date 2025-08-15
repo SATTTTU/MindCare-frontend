53a31cd4f013de3c6a3cb0d0e57ca6a6d11b9d06
SATTTTU <timalsinasatish430@gmail.com>
2025-08-15 20:39:05 +0545

    fjfjfjfjj

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { AppProvider } from "./app/provider"; // Your global provider (e.g., for theme, query client)
+import { AppProvider } from "./app/provider"; 
 import { createAppRouter } from "./app/router";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
